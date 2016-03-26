fd6b3f4848377ccbed6da7afbc9e921f3f296940
manudevelopia <manugarciarodriguez@gmail.com>
2016-03-26 11:37:15 +0100

    Added brew package list

--- linkMake.sh
+++ linkMake.sh
@@ -4,3 +4,7 @@ ln -s ~/.vimrc ~/.config/nvim/init.vim
 
 # Neovim
 ln -s ~/Developer/Config/nvim ~/.config/nvim
+
+# Vim
+ln -s ~/Development/Config/vim/vimrc ~/.vimrc
+ln -s ~/Development/Config/vim ~/.vim
